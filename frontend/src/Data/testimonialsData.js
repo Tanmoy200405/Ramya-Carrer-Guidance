7e79fb6d68135df52f7f2b9890e9dabbc16ae2d9
Tanmoy05 <tanmoymukherjee746@gmail.com>
2026-04-26 20:30:20 +0530

    Add new testimonials for Aysika Dey and Sambhabi Banerjee with photos

--- frontend/src/Data/testimonialsData.js
+++ frontend/src/Data/testimonialsData.js
@@ -1,29 +1,39 @@
 import photo1 from "../assets/testimonial/photo1.jpeg";
 import photo2 from "../assets/testimonial/photo2.jpeg";
+import photo3 from "../assets/testimonial/photo3.jpeg";
+import photo4 from "../assets/testimonial/photo4.jpeg";
 
 export const testimonialsData = [
-  {
-    id: 1,
-    name: "Tanmoy Mukherjee",
-    college: "St Thomas College of Engineering & Technology",
-    review: "Ramya Career Guidance helped me find the perfect course. Their counseling was extremely thorough and professional.",
-    photo: photo1,
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "Moubani Issu",
-    college: "Bhawanipur Global Campus ",
-    review: "I liked it very much. Their guidance helped me in moving forward in the future and assisted me regarding college.",
-    photo: photo2, 
-    rating: 5
-  },
-  {
-    id: 3,
-    name: "Priya Das",
-    college: "iLEAD",
-    review: "The admission process was so smooth because of their constant support. Truly a sanctuary for students.",
-    photo: photo1, 
-    rating: 5
-  }
+    {
+        id: 1,
+        name: "Tanmoy Mukherjee",
+        college: "St Thomas College of Engineering & Technology",
+        review: "Ramya Career Guidance helped me find the perfect course. Their counseling was extremely thorough and professional.",
+        photo: photo1,
+        rating: 5
+    },
+    {
+        id: 2,
+        name: "Moubani Issu",
+        college: "Bhawanipur Global Campus ",
+        review: "I liked it very much. Their guidance helped me in moving forward in the future and assisted me regarding college.",
+        photo: photo2, 
+        rating: 5
+    },
+    {
+        id: 3,
+        name: "Aysika Dey",
+        college: "Bhawanipur Global Campus",
+        review: "The counselor helped me to choose the right platform and obviously the right college which I deserve... The guidance was 10 out of 10..",
+        photo: photo3,
+        rating: 5
+    },
+    {
+        id: 4,
+        name: "Sambhabi Banerjee",
+        college: "Bhawanipur Global Campus",
+        review: "I have benefited greatly from their guidance. It has helped me set my goals in life and find a good college.",
+        photo: photo4,
+        rating: 5
+    }
 ];
